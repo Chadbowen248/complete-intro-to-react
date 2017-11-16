903fd9bb5e6e78a25a9af8fc58b4890926e6364d
Chad Bowen <Chadbowen248@gmail.com>
2017-11-15 19:33:45 -0500

    search is working with a form, need to change it so i can work with data coming back

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-
+import React from "react"
+import Axios from "axios"
 
 class Search extends React.Component {
-  render(){
-    return(
-      <form action="http://localhost:3000/test" method="get">
-        <input name="search_term"/>
+  render() {
+    return (
+      <form action="http://localhost:3000/comicvine_api" method="get">
+        <input
+          name="search"
+          type="text"
+          ref={input => {
+            this.textInput = input
+          }}
+        />
         <button>click me</button>
       </form>
     )
   }
 }
 
-export default Search;
-
+export default Search
 
 // this should be where you add comics from comic vine
+// ;<input
+//   className="comicSearch"
+//   onFocus={() => {
+//     this.setState({ results: [], temp: [], flag: false, fade: false, images: 0 })
+//     this.textInput.value = ""
+//   }}
--- js/server.js
+++ js/server.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require("express")
 const bodyParser = require("body-parser")
+const Axios = require("axios")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Orgin", "*");
-  res.header("Access-Control-Allow-Orgin", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Orgin", "*")
+  res.header("Access-Control-Allow-Orgin", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
-app.get("/test", (req, res) => {
-  const testing = req.body.search_term
-  console.log(req.body)
-  res.send(testing)
-});
+app.get("/comicvine_api", (req, res) => {
+  const searchTerm = req.query.search
+  const apiKey = "2736f1620710c52159ba0d0aea337c59bd273816"
+  const URL = `https://comicvine.gamespot.com/api/search/?api_key=${apiKey}&format=json&query=${searchTerm}&resources=volume`
+  Axios.get(URL).then(response => console.log(response.data.results))
+})
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"))
