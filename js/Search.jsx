6b9f3d3bfc34cb8a79af6d9fd55ca36476b00d70
Chad Bowen <Chadbowen248@gmail.com>
2017-11-18 20:05:33 -0500

    code clean up and state search results added to search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,19 +2,15 @@ import React from "react"
 import Axios from "axios"
 
 class Search extends React.Component {
-  state = { searchTerm: "" }
-  // searchComic = e => {
-  //   e.preventDefault()
-  //   alert("eaah")
-  // }
+  state = {
+    searchTerm: "",
+    results: []
+  }
 
   searchForComic = () => {
     const searchTerm = this.textInput.value
-    // const apiKey = "2736f1620710c52159ba0d0aea337c59bd273816"
-    // const URL = `https://comicvine.gamespot.com/api/search/?api_key=${apiKey}&format=json&query=${searchTerm}&resources=volume`
-    // this.setState({ loaded: true, fade: false })
-    // Axios.get(`http://localhost:3000/comicvine_api`).then(res => res.data.results).then(results => this.setState({ results }))
-    Axios.get(`http://localhost:3000/comicvine_api?search_term=${searchTerm}`).then(res => console.log(res.data))
+    Axios.get(`http://localhost:3000/comicvine_api?search_term=${searchTerm}`)
+    .then(results => this.setState({results: results.data}))
   }
   render() {
     return (
@@ -26,19 +22,10 @@ class Search extends React.Component {
             this.textInput = input
           }}
         />
-        <input className="comic-search__submit" type="submit" onClick={this.searchForComic} />
+        <input type="submit" onClick={this.searchForComic} />
       </div>
     )
-    // <form action="http://localhost:3000/comicvine_api" method="get">
-
-    //   <button>click me</button>
-    // </form>
   }
 }
 
 export default Search
-
-
-// handleSearchTermChange = event => {
-//   this.setState({ searchTerm: event.target.value })
-// }
--- js/server.js
+++ js/server.js
@@ -3,22 +3,12 @@ const cors = require('cors');
 const Axios = require("axios")
 
 const app = express()
-
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Orgin", "*")
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
 app.use(cors()); 
 
 app.get("/comicvine_api", (req, res) => {
   const searchTerm = req.query.search_term
-  console.log(req.query)
   const apiKey = "2736f1620710c52159ba0d0aea337c59bd273816"
-  // const URL = `https://comicvine.gamespot.com/api/search/?api_key=${apiKey}&format=json&query=batman&resources=volume`
   const URL = `https://comicvine.gamespot.com/api/search/?api_key=${apiKey}&format=json&query=${searchTerm}&resources=volume`
-  console.log(URL)
   Axios.get(URL).then(response => res.send(response.data.results))
 })
 
