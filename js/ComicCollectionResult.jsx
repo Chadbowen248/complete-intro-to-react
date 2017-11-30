f2770af497053ff359731c87bd8931b8314833cf
Chad Bowen <chadbowen248@gmail.com>
2017-11-30 15:05:58 -0500

    comic vine search working, need to add add function

--- js/ComicCollectionResult.jsx
+++ js/ComicCollectionResult.jsx
@@ -1,18 +1,36 @@
 import React from "react"
+import styled from "styled-components";
 import { shape, func, number } from "prop-types"
 
+const SingleComic = styled.div`
+width: 23%;
+margin-left: 1%;
+border: 1px solid #ccc;
+padding: 1em;
+margin-top: .5em;
+margin-bottom: .5em;
+margin-right: 1%;
+`;
+const ComicImage = styled.img`
+  width: 100%;
+  height: 85%;
+`;
+const ComicTitle = styled.p`
+font-size: 1vw;
+font-family: 'Fjalla One', sans-serif;
+color: #333;
+`;
 const ComicCollectionResult = props =>
   // const addButton = props.details.count_of_issues === 1 ? 'Add Me' : 'disabled';
-  <div className="comic-container__comic" key={props.details.id}>
-    <img
-      className="comic-results__image"
+  <SingleComic>
+    <ComicImage
       src={props.details.image.medium_url}
       alt=""
       onLoad={() => props.haveImagesLoaded("results")}
     />
-    <p className="comic-container__title">
+    <ComicTitle>
       {props.details.name}
-    </p>
+    </ComicTitle>
     <button
       className={props.isOnlyIssue === 1 ? "comic-results-add-button" : "comic-results-add-button__disabled"}
       onClick={() => props.addComic(props.details)}
@@ -20,7 +38,7 @@ const ComicCollectionResult = props =>
       Add Comic
     </button>
 
-    <button
+    {/* <button
       className={props.isOnlyIssue > 1 ? "comic-results-add-button" : "comic-results-add-button__disabled"}
       onClick={() =>
         props.deeperSearch(
@@ -28,16 +46,16 @@ const ComicCollectionResult = props =>
         )}
     >
       go deeper
-    </button>
+    </button> */}
     <div className="comic-vine-info">
-      <a href={props.details.site_detail_url}>
+      {/* <a href={props.details.site_detail_url}>
         <img className="cvlogo" src="public/img/cvlogo.png" alt="" />
-      </a>
+      </a> */}
       <span className="comic-container__title">
         Issues in volume: {props.details.count_of_issues}
       </span>
     </div>
-  </div>
+  </SingleComic>
 
 export default ComicCollectionResult
 
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,20 +1,52 @@
-import React from "react"
-import Axios from "axios"
-import ComicCollectionResult from "./ComicCollectionResult"
+import React from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import ComicCollectionResult from "./ComicCollectionResult";
+
+const ComicWrapper = styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 100%;
+margin-top: 1.5em;
+margin-bottom: 1.5em;
+justify-content: flex-start;
+padding-right: .5em;
+padding-left: .5em;
+`;
+
+const ComicSearchComicVine = styled.input`
+margin-top: 1.5em;
+width: 100%;
+height: 50px;
+text-align: center;
+`;
+const SubmitButton = styled.input`
+background-color: transparent;
+margin:30px auto;
+width: 200px;
+display: block;
+padding: 5px;
+`;
 
 class Search extends React.Component {
   state = {
     searchTerm: "",
     results: []
-  }
+  };
+  handleChange = event => {
+    this.setState({ searchTerm: event.target.value });
+    console.log(this.state.searchTerm);
+  };
 
   searchForComic = () => {
-    const searchTerm = this.textInput.value
-    Axios.get(`http://localhost:3000/comicvine_api?search_term=${searchTerm}`)
-    .then(results => this.setState({results: results.data}))
-  }
+    const searchTerm = this.state.searchTerm;
+    console.log(searchTerm);
+    Axios.get(
+      `http://localhost:3000/comicvine_api?search_term=${searchTerm}`
+    ).then(results => this.setState({ results: results.data }));
+  };
   render() {
-    const firstSearch = this.state.results.map(comic =>
+    const firstSearch = this.state.results.map(comic => (
       <ComicCollectionResult
         key={comic.id}
         details={comic}
@@ -24,21 +56,21 @@ class Search extends React.Component {
         haveImagesLoaded={this.haveImagesLoaded}
         modal={this.state.modal}
       />
-    )
+    ));
     return (
       <div>
-        <input
+        <ComicSearchComicVine
           name="search"
           type="text"
-          ref={input => {
-            this.textInput = input
-          }}
+          onChange={this.handleChange}
         />
-        <input type="submit" onClick={this.searchForComic} />
-        {firstSearch}
+        <SubmitButton type="submit" onClick={this.searchForComic} asdf/>
+        <ComicWrapper>
+          {firstSearch}
+        </ComicWrapper>
       </div>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
